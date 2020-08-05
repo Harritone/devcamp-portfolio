0afe9b45c94af1b6ea2198ec9c389731e845b3d8
Harritone <yourrasik@gmail.com>
2020-08-05 21:29:17 +1000

    Intrgrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    IdnilovViewTool::Renderer.copyright 'Iurii Danilov', 'All rights reserved'
+  end
 end
